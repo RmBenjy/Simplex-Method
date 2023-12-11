5bd544961837ca903b2a737de7fb82ff3449eba5
Reda Benjelloun <136175741+RmBenjy@users.noreply.github.com>
2023-12-11 17:19:02 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,172 +1,171 @@
 int main() {
 
 
-    int nc, nv;
-    int g;
-    printf("**********************WELCOME**********************\n");
-    printf("Choisir le type de probleme :  \n");
-    printf("1. Maximisation.\n");
-    printf("2. Minimisation.\n");
-    scanf("%d",&g);getchar();
+     int nc, nv;
+     int g;
+     printf("**********************WELCOME**********************\n ");
+     printf("Choose problem type: \n");
+     printf("1. Maximization.\n");
+     printf("2. Minimization.\n");
+     scanf("%d",&g); getchar();
 
-    switch(g)
-    {
-        case 1:{
-    printf("Entrez le nombre de variables : ");
-    scanf("%d", &nv);
-    printf("Entrez le nombre de contraintes : ");
-    scanf("%d", &nc);
-
-
-
-	int col = nv + nc + 1 ;
-	int ligne = nc + 1 ;
-
-    int i ,j;
-    int solution[nv] ;
-
-
-    for( i = 0 ; i < nv ; i++)
-    		{solution[i] = -1 ;}
-
-    float **matrice=(float**)malloc(ligne * sizeof(float*));
-    for (i=0; i < ligne; i++) {
-        matrice[i] = (float*)calloc(col , sizeof(float));
-    }
-
-
-    printf("Entrez les coefficients de la fonction objectif Z :\n");
-
-
-    for (i = 1; i <= nv; i++){
-    	printf("X%d  : ",i) ;
-        scanf("%f", &matrice[ligne-1][i]);
-    	}
-    	matrice[ligne-1][0]=0; //second membre de z
-    	printf("\n\n") ;
+     switch(g)
+     {
+         case 1:{
+     printf("Enter the number of variables: ");
+     scanf("%d", &nv);
+     printf("Enter the number of constraints: ");
+     scanf("%d", &nc);
 
 
 
+    int col = nv + nc + 1;
+    int row = nc + 1;
 
-    printf("Entrez les coefficients des contraintes :\n");
-
-
-    for (i = 0; i < ligne-1; i++) {
-        printf("Contrainte %d : \n", i + 1);
-        for (j = 1; j <= nv; j++){
-
-        	printf("X%d   :", j) ;
-            scanf("%f", &matrice[i][j]);
-    	}
-
-    	printf("\nEntrez le second membre : ") ;
-    	scanf("%f", &matrice[i][0]);
-    	matrice[i][nv+i+1] = 1 ;
-    	printf("\n");
-    }
+     int i,j;
+     int solution[nv];
 
 
+     for(i = 0; i < nv; i++)
+     {solution[i] = -1;}
 
-    printf("\n\n") ;
+     float **matrix=(float**)malloc(row * sizeof(float*));
+     for (i=0; i < row; i++) {
+         matrix[i] = (float*)calloc(col, sizeof(float));
+     }
 
-    affiche(matrice , ligne , col) ;
 
-    methode_simplex(matrice , ligne, col,solution);
+     printf("Enter the coefficients of the objective function Z:\n");
 
-		for(int i = 0 ; i <nv; i++){
-			if(solution[i] == -1 ){
-				printf("   X%d = 0   ",i+1) ;
-			}
-			else{
 
-                printf("   X%d = %.2f    ",i+1,matrice[solution[i]][0]) ;
-			}
-			}
+     for (i = 1; i <= nv; i++){
+     printf("X%d: ",i);
+         scanf("%f", &matrix[line-1][i]);
+     }
+     matrix[row-1][0]=0; //second member of z
+     printf("\n\n");
 
 
 
-    printf("     \n\nZ = %.2f    \n\n",-matrice[ligne-1][0]);
-    break;}
+
+     printf("Enter the constraint coefficients:\n");
+
+
+     for (i = 0; i < row-1; i++) {
+         printf("Constraint %d: \n", i + 1);
+         for (j = 1; j <= nv; j++){
+
+         printf("X%d:", j);
+             scanf("%f", &matrix[i][j]);
+     }
+
+     printf("\nEnter the second member: ");
+     scanf("%f", &matrix[i][0]);
+     matrix[i][nv+i+1] = 1;
+     printf("\n");
+     }
 
 
 
-    case 2:{printf("Entrez le nombre de variables : ");
-    scanf("%d", &nv);
-    printf("Entrez le nombre de contraintes : ");
-    scanf("%d", &nc);
+     printf("\n\n");
+
+     displays(matrix, row, col);
+
+     simplex_method(matrix, row, col, solution);
+
+     for(int i = 0; i <nv; i++){
+	if(solution[i] == -1 ){
+		printf("X%d = 0",i+1);
+	}
+	else{
+
+                printf("X%d = %.2f",i+1,matrix[solution[i]][0]);
+	}
+     }
+
+
+     printf(" \n\nZ = %.2f \n\n",-matrix[row-1][0]);
+     break;}
 
 
 
-	int col = nv + nc + 1 ;
-	int ligne = nc + 1 ;
-
-    int i ,j;
-    int solution[nv] ;
-
-
-    for( i = 0 ; i < nv ; i++)
-    		{solution[i] = -1 ;}
-
-    float **matrice=(float**)malloc(ligne * sizeof(float*));
-    for (i=0; i < ligne; i++) {
-        matrice[i] = (float*)calloc(col , sizeof(float));
-    }
-
-
-    printf("Entrez les coefficients de la fonction objectif Z :\n");
-
-
-    for (i = 1; i <= nv; i++){
-    	printf("X%d  : ",i) ;
-        scanf("%f", &matrice[ligne-1][i]);
-    	}
-    	matrice[ligne-1][0]=0; //second membre de z
-    	printf("\n\n") ;
-
-
-    printf("Entrez les coefficients des contraintes :\n");
-
-
-    for (i = 0; i < ligne-1; i++) {
-        printf("Contrainte %d : \n", i + 1);
-        for (j = 1; j <= nv; j++){
-
-        	printf("X%d   :", j) ;
-            scanf("%f", &matrice[i][j]);
-    	}
-
-    	printf("\nEntrez le second membre : ") ;
-    	scanf("%f", &matrice[i][0]);
-    	matrice[i][nv+i+1] = 1 ;
-    	printf("\n");
-    }
+     case 2:{printf("Enter the number of variables: ");
+     scanf("%d", &nv);
+     printf("Enter the number of constraints: ");
+     scanf("%d", &nc);
 
 
 
-    printf("\n\n") ;
-    rendre_matrice_negative(matrice , ligne, col);
-    affiche(matrice , ligne , col) ;
+    int col = nv + nc + 1;
+    int line = nc + 1;
 
-    methode_simplex1(matrice , ligne, col,solution);
-
-		for(int i = 0 ; i <nv; i++){
-			if(solution[i] == -1 ){
-				printf("   X%d = 0   ",i+1) ;
-			}
-			else{
-
-                printf("   X%d = %.2f    ",i+1,-matrice[solution[i]][0]) ;
-			}
-			}
+     int i,j;
+     int solution[nv];
 
 
+     for(i = 0; i < nv; i++)
+     {solution[i] = -1;}
 
-    printf("     \n\nZ = %.2f    \n\n",-matrice[ligne-1][0]);
-    break;}
-
-    default :break;}
-
- return 0;
+     float **matrix=(float**)malloc(row * sizeof(float*));
+     for (i=0; i < row; i++) {
+         matrix[i] = (float*)calloc(col, sizeof(float));
+     }
 
 
-    }
+     printf("Enter the coefficients of the objective function Z:\n");
+
+
+     for (i = 1; i <= nv; i++){
+         printf("X%d: ",i);
+         scanf("%f", &matrix[row-1][i]);
+     }
+	     
+     matrix[row-1][0]=0; //second member of z
+     printf("\n\n");
+
+
+     printf("Enter the constraint coefficients:\n");
+
+
+     for (i = 0; i < row-1; i++) {
+         printf("Constraint %d: \n", i + 1);
+         for (j = 1; j <= nv; j++){
+
+             printf("X%d:", j);
+             scanf("%f", &matrix[i][j]);
+          }
+
+         printf("\nEnter the second member: ");
+         scanf("%f", &matrix[i][0]);
+         matrix[i][nv+i+1] = 1;
+         printf("\n");
+     }
+
+
+
+     printf("\n\n");
+     make_matrix_negative(matrix, row, col);
+     displays(matrix, row, col);
+
+     method_simplex1(matrix, row, col, solution);
+
+     for(int i = 0; i <nv; i++){
+	if(solution[i] == -1 ){
+		printf("X%d = 0",i+1);
+	}
+        else{
+
+                 printf("X%d = %.2f",i+1,-matrix[solution[i]][0]);
+        }
+     }   
+
+
+
+     printf(" \n\nZ = %.2f \n\n",-matrix[row-1][0]);
+     break;}
+
+     default :break;}
+
+  return 0;
+
+}
